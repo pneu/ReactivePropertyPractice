ad3e3e9ed5f58d72e7223ad1b4b90b95174ad78a
pneu <pneumaster@gmail.com>
2017-01-29 01:38:28 +0900

    initial commit

---
+++ ReactivePropertyPractice/PersonViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Reactive.Bindings;
+
+namespace ReactivePropertyPractice
+{
+    public class PersonViewModel
+    {
+        public ReactiveProperty<string> Name { get; } = new ReactiveProperty<string>();
+    }
+}
